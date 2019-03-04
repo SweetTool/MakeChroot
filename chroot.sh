c9697a198d3a309273baf0a415e2d77735b46a90
sunny <784911204@qq.com>
2019-03-04 09:11:08 +0800

    <new> first commit, initialize the project.

---
+++ chroot.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+CHROOT=/home/chroot
+for i in $( ldd $* | grep -v dynamic | cut -d " " -f 3 | sed 's/://' | sort | uniq )
+  do
+      cp --parents $i $CHROOT
+  done
+
+# ARCH amd64
+if [ -f /lib64/ld-linux-x86-64.so.2 ]; then
+   cp --parents /lib64/ld-linux-x86-64.so.2 /$CHROOT
+fi
+
+# ARCH i386
+if [ -f  /lib/ld-linux.so.2 ]; then
+   cp --parents /lib/ld-linux.so.2 /$CHROOT
+fi
+
+echo "Chroot jail is ready. To access it execute: chroot $CHROOT"
